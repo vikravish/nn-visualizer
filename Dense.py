f804525f7b7cd3f54e0b738e4512afa574f4c841
Vikram <vikravishankar@gmail.com>
2026-04-28 05:47:25 -0400

    Added ReLU activation function implementation

--- Dense.py
+++ Dense.py
@@ -26,11 +26,23 @@ class Dense:
         
         # Return next layer via matrix multiplication (@ operator) between + bias
         return X @ self.W + self.b
+
+# ReLU class in order to apply ReLU activation function to a Dense activation vector
+# Incorporating ReLU avoids linearity, therefore making the use of multiple layers meaningful
+# A "ReLU" is a vector in which the activations undergo ReLU transformation
+class ReLU:
+    def forward(self, Z: np.ndarray) -> np.ndarray:
+        self.Z = Z
+        # Returns an augmented Z in which any activation < 0 gets treated as 0
+        return np.maximum(0,Z)
     
 X = np.random.randn(1,784) # Fake input data for testing -> 1 row x 784 cols
 layer1 = Dense(784, 128) # Layers are hardcoded
 z1 = layer1.forward(X) # Computes matrix-vector mutiplication between output weights matrix and input vector
+relu = ReLU() # Initialize a ReLU vector
+z1_relu = relu.forward(z1) # Apply ReLU function to z1 and store at z1_relu
 
 print("Input shape:", X.shape)
 print("Weights shape:", layer1.W.shape)
 print("Bias shape:", layer1.b.shape)
+print(z1_relu)
